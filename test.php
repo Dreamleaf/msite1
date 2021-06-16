aeaa7a077e09a429bab7eff8ca4ef26795fb2d40
Dreamleaf <nik@dreamleaf.co.uk>
2021-06-16 12:37:21 +0100

    Removing test file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "msite1", 3306);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-
-echo $mysqli->host_info . "\n";
-$mysqli->close();
-?>
